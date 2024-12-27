48b15cdcb07f2962ebeb26ffe408e817386d2724
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2024-12-28 00:00:25 +0700

    Ultilized jQuery for (1) Loading screen with dynamic images. (2) Added post-loading screen animations!!!

--- Assets/script/loader.js
+++ Assets/script/loader.js
@@ -1,5 +1,46 @@
 $(window).on('load', function() {
     setTimeout(function() {
-        $(".loader--wrapper").fadeOut()
+        $(".loader--wrapper").slideUp(1200);
+        $(".loader").fadeOut(600);
+        $(".header").fadeOut(0)
+        $(".content__video").fadeOut(0)
+        $(".content__navbar").fadeOut(0)
+        $(".footer").fadeOut(0)
+        setTimeout(function(){
+            $(".header").fadeIn(1200)
+        },1200)
+        setTimeout(function(){
+            $(".content__navbar").fadeIn(1000)
+        },2400)
+        setTimeout(function(){
+            $(".content__video").fadeIn(1000)
+        },3400)
+        setTimeout(function(){
+            $(".footer").fadeIn(1200)
+        },4600)
     }, 2000);
 });
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+return array;
+};
+var images = [
+    { src : 'Other_Files/angry beako v2.gif'},
+    { src : 'Other_Files/spinning beako.gif'},
+    { src : 'Other_Files/beako super surprised (gif).gif'},
+    { src : 'Other_Files/Puck clapping.gif'},
+    { src : 'Other_Files/fuck you leave me alone (gif).gif'},
+    { src : 'Other_Files/angry beatrice gif.gif'},
+
+];
+shuffleArray(images)
+var img, div;
+var cont = $('.loader');
+cont.children().remove();
+img = $('<img />', { src : images[0].src, width: "auto", height: "160px" });
+div = $('<div></div>', { class : 'loader' });
+div.append(img);
+cont.append(div);
