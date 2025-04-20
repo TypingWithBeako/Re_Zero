3102a247d0696af259a9c34b2591b1c8b5148bdd
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-20 14:59:03 +0700

    fix: Using "Times New Roman" for the header instead of "Playfair Display"

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v1';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v2';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
