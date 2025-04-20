b9673f824ccac7006f50561e1f6e2281a42db3aa
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-20 09:19:18 +0700

    chore: Changed keyboard modal layouts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-v2';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-v3';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
