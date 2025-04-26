bd20171924edd22968745a9dba1377610ddba80f
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-26 23:09:29 +0700

    revert: Figured out that #7771b3 fits better so revert changes :V

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v4';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v5';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
