dfc0f2b0c4358b6271a2a581529ecfc473570c17
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-27 12:29:23 +0700

    chore: Translating remaining Vietnamese text

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v5';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v6';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
