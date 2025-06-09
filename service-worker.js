4791777c7a8020feae5acf1b28b276ca783e3ea0
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-06-09 08:21:45 +0700

    chore: Responsive changes.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v26';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-EN-v27';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
